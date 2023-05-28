046d2b408387ab482bac3db9a4bba114ddc87c5f
HodowDs <dekuortizv@gmail.com>
2023-05-28 18:14:27 -0500

    carta 2 y corrección

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ window.onload = function() {
     var currentDate = new Date();
     var timeDifference = currentDate - targetDate;
 
-    var seconds = Math.floor(timeDifference / 1000) % 60;
-    var minutes = Math.floor(timeDifference / (1000 * 60)) % 60;
-    var hours = Math.floor(timeDifference / (1000 * 60 * 60)) % 24;
-    var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) % 30.500 ; // Aproximación promedio de días por mes
-    var weeks = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) % 52;
-    var months = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30.4375)) % 12;
-    var years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30.4375 * 12));
+    var years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365));
+    var months = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30.436875)); // Promedio de días en un mes (365.25 / 12)
+    var weeks = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 7));
+    var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
     document.getElementById("timer").innerHTML = "Han pasado " + years + " años, " + months + " meses, "  + weeks + " semanas, " + days + " días, " + hours + " horas, " + minutes + " minutos y " + seconds + " segundos desde el día que tú y yo somos novios";
 
