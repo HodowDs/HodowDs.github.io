da729413fe2717785f631423a1b1ce76855f4b48
hodowds <134245607+hodowds@users.noreply.github.com>
2024-08-06 19:56:33 -0500

    Add files via upload

---
+++ Misterio/misterio.js
@@ -0,0 +1,31 @@
+// Establece la fecha a la que estamos contando hacia atrás
+var countDownDate = new Date("Oct 27, 2024 00:00:00").getTime();
+
+// Actualiza la cuenta regresiva cada 1 segundo
+var countdownFunction = setInterval(function() {
+
+    // Obtiene la fecha y hora actuales
+    var now = new Date().getTime();
+
+    // Calcula la distancia entre ahora y la fecha de cuenta regresiva
+    var distance = countDownDate - now;
+
+    // Cálculos de tiempo para días, horas, minutos y segundos
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Muestra el resultado en un elemento con id="countdown"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // Si la cuenta regresiva ha terminado, redirige a otra página
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+        window.location.href = "https://www.ejemplo.com"; // Cambia esto a la URL de destino
+    }
+}, 1000);
