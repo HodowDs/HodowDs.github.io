6d78826c04784bf91c1a0377cde3839b3339361b
hodowds <134245607+hodowds@users.noreply.github.com>
2024-10-18 14:59:35 -0500

    Update codigo.js

--- Misterio/codigo.js
+++ Misterio/codigo.js
@@ -1,1 +1,13 @@
+function checkCode() {
+    const correctCode = "filepe + sofu = 2710";
+    const codeInput = document.getElementById('codeInput').value;
+    const message = document.getElementById('message');
+
+    if (codeInput === correctCode) {
+        window.location.href = "https://hodowds.github.io/Misterio/test.html";
+    } else {
+        message.textContent = "Ese no es, animo amor.";
+        message.style.color = 'pink';
+    }
+}
 
